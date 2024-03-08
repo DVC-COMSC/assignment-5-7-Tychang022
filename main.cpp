2fa722f40dc279741f58a25d84562bd8577d6386
Tychang022 <156855881+Tychang022@users.noreply.github.com>
2024-03-08 07:08:43 +0000

    TC

--- main.cpp
+++ main.cpp
@@ -3,13 +3,25 @@ using namespace std;
 
 int main()
 {
-	int n1, n2;
+	int n1, n2, i, n3, n4, sum = 0;
 	int N;
-
+	cout << "Enter two initial values\n";
 	cin >> n1 >> n2;
+	do {
+	cout << "Enter the number of sequences\n";
 	cin >> N;
-
-	/***************************************************
-	 * Code your program here
-	 **************************************************	*/
-}
+		if (N <= 2)
+			cout << "Invalid Input. Please try again\n";
+	}while( N <= 2);
+	cout << n1 << " " << n2 << " ";
+	
+	for (i = 3; i <= N; i++)
+		{
+			n3 = n2 + n1;
+			cout << n3 << " ";
+			n1 = n2;
+			n2 = n3;
+		}
+		return 0;
+	}
+	
